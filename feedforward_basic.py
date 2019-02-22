c72ce1b9a4b1097bca2e62dba31e8c21030406a2
Harmit Jasani <harmitrox@ymail.com>
2019-02-21 23:14:28 -0600

    Update feedforward_basic.py

--- feedforward_basic.py
+++ feedforward_basic.py
@@ -1,39 +1,26 @@
 import numpy as np
 
 
-class NeuralNetwork():
+class FeedForwardNeuralNetwork():
 
     def __init__(self):
-        # seeding for random number generation
         np.random.seed(0)
-
-        # converting weights to a 3 by 1 matrix with values from -1 to 1 and mean of 0
         self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
-
+        
     def sigmoid(self, x):
-        # applying the sigmoid function
         return 1 / (1 + np.exp(-x))
 
     def sigmoid_derivative(self, x):
-        # computing derivative to the Sigmoid function
         return x * (1 - x)
 
     def train(self, training_inputs, training_outputs, training_iterations):
-        # training the model to make accurate predictions while adjusting weights continually
         for iteration in range(training_iterations):
-            # siphon the training data via  the neuron
-            output = self.think(training_inputs)
-
-            # computing error rate for back-propagation
+            output = self.understand(training_inputs)
             error = training_outputs - output
-
-            # performing weight adjustments
             adjustments = np.dot(training_inputs.T, error * self.sigmoid_derivative(output))
-
             self.synaptic_weights += adjustments
 
-    def think(self, inputs):
-
+    def understand(self, inputs):
         inputs = inputs.astype(float)
         output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
         return output
@@ -42,10 +29,9 @@ class NeuralNetwork():
 if __name__ == "__main__":
     neural_network = NeuralNetwork()
 
-    print("Beginning Randomly Generated Weights: ")
+    print("Initial Randomly Generated Weights: ")
     print(neural_network.synaptic_weights)
-
-    # training data having of 4 examples--3 input values and 1 output
+    
     training_inputs = np.array([[0, 0, 1],
                                 [1, 1, 1],
                                 [1, 0, 1],
@@ -55,7 +41,6 @@ if __name__ == "__main__":
 
     training_outputs = np.array([[0, 1, 1, 0, 1, 0]]).T
 
-    # training
     neural_network.train(training_inputs, training_outputs, 100)
 
     print("Ending Weights After Training: ")
